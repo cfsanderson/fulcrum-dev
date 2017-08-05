66f6c31127c8779e58d7fb69829f51f2f5ac8734
Caleb Sanderson <calebsanderson@gmail.com>
2017-08-04 21:33:12 -0400

    checking in

--- public/index.js
+++ public/index.js
@@ -28,3 +28,18 @@
 //   module.hot.dispose(() => window.location.reload())
 //   module.hot.accept(err => console.error(err))
 // }
+
+import './styles/screen.scss'
+
+const main = () => {
+  document.querySelector('h1').textContent += ''
+}
+
+document.addEventListener('DOMContentLoaded', main)
+
+// HERE BE DRAGONS... and webpack. Don't touch.
+if (process.env.NODE_ENV !== 'production') require('./index.html')
+if (module.hot) {
+  module.hot.dispose(() => window.location.reload())
+  module.hot.accept(err => console.error(err))
+}
